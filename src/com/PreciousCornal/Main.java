7a9e24c1f590458c2f38e552792041abe9dc904e
eltoro07 <preciousmaecornal@gmail.com>
2024-09-15 11:40:10 +0900

    added a diamond star pattern

--- src/com/PreciousCornal/Main.java
+++ src/com/PreciousCornal/Main.java
@@ -12,15 +12,30 @@ public class Main {
 
         // for increasing pattern/space -> int j=1; j <= i
         //for decreasing pattern/space -> int k=i;k<=number
-        for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= i; j++)
-                System.out.print(" ");
-            for (int k = i; k <= number; k++)
-                System.out.print("*");
-
+        for (int i = 1; i < number; i++) {
+            for (int j = i; j <= number; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k < i; k++) {
+                System.out.print("*" + " ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*" + " ");
+            }
             System.out.println();
             }
-
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = i; k < number; k++) {
+                System.out.print("*" + " ");
+            }
+            for (int k = i; k <= number; k++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
         }
     }
 
